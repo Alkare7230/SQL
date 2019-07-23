fa781cb4444c24279c56b7ae0429246700d29c9f
Alkare7230 <52501436+Alkare7230@users.noreply.github.com>
2019-07-23 16:55:51 +0200

    Add files via upload

---
+++ contraint.sql
@@ -0,0 +1,7 @@
+-- constrint qui est sur data-alter
+
+use store;
+
+PRIMARY KEY (`customerNumber`),
+  KEY `salesRepEmployeeNumber` (`salesRepEmployeeNumber`),
+  CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`salesRepEmployeeNumber`) REFERENCES `employees` (`employeeNumber`)
